2200635bb9e67f2db8b201fd4434884e06526968
calscoo <scoutscott13@gmail.com>
2025-03-10 18:48:31 -0700

    disable easy downloads

--- hugo/themes/gallery/assets/js/main.js
+++ hugo/themes/gallery/assets/js/main.js
@@ -3,3 +3,22 @@ import "./gallery.js";
 import "./lazysizes.js";
 import "./lightbox.js";
 import "./slider.js";
+
+// Prevent right-click on images
+document.addEventListener('DOMContentLoaded', function() {
+  // Disable right-click on all images
+  document.addEventListener('contextmenu', function(e) {
+    if (e.target.tagName === 'IMG') {
+      e.preventDefault();
+      return false;
+    }
+  }, false);
+  
+  // Disable dragging of images
+  document.addEventListener('dragstart', function(e) {
+    if (e.target.tagName === 'IMG') {
+      e.preventDefault();
+      return false;
+    }
+  }, false);
+});
