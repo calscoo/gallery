5dae20bbb9c90a960b28f8b76ca1bbc668bb7d86
calscoo <scoutscott13@gmail.com>
2025-05-04 22:38:55 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 resource "aws_amplify_app" "gallery" {
   name       = "gallery"
   repository = "https://github.com/calscoo/gallery"
@@ -35,7 +34,7 @@ applications:
   environment_variables = {
     AMPLIFY_DIFF_DEPLOY       = "false"
     AMPLIFY_MONOREPO_APP_ROOT = "hugo"
-    _LIVE_UPDATES             = "[{\"name\":\"Hugo\",\"pkg\":\"hugo\",\"type\":\"hugo\",\"version\":\"latest\"}]"
+    _LIVE_UPDATES             = "[{\"name\":\"Hugo\",\"pkg\":\"hugo\",\"type\":\"hugo\",\"version\":\"0.145\"}]"
   }
 }
 
